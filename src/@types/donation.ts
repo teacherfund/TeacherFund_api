4ec0b81f89e61792d005b9f5a0b9565cc46f76b4
Joel Wasserman <joel.wasserman8@gmail.com>
2019-01-06 18:25:55 -0800

    added in donation controller

--- src/@types/donation.ts
+++ src/@types/donation.ts
@@ -2,6 +2,7 @@ export interface Donation {
   id: number,
   amount: number,
   frequency: string,
+  userId: number,
   meta: any
 }
 
---
+++ src/controllers/donation/donation.ts
@@ -0,0 +1,57 @@
+const sqlModels = require('../../models')
+import {Donation, Frequency} from '../../@types/donation'
+
+export interface CreateDonationBody {
+  userId: number,
+  frequency: Frequency,
+  amount: number,
+  date: Date
+}
+
+export interface UpdateDonationBody {
+  id?: number
+  frequency?: Frequency,
+  amount: number
+}
+
+export const createNewDonation = (body: CreateDonationBody): Promise<Donation> => {
+  try {
+    return sqlModels.Donation.create(body)
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
+
+export const getDonation = (id: string): Promise<Donation> => {
+  try {
+    return sqlModels.Donation.find({ where: {id: id}})
+  } catch (e) {
+    return Promise.reject (e)
+  }
+}
+
+export const getAllDonations = (): Promise<Donation[]> => {
+  try {
+    return sqlModels.Donation.findAll()
+  } catch (e) {
+    return Promise.reject (e)
+  }
+}
+
+export const updateDonation = async (body: UpdateDonationBody): Promise<Donation> => {
+  try {
+    const localDonation = await sqlModels.Donation.find({ where: {id: body.id }})
+    return localDonation.save(body)
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
+
+export const deleteDonation = async (body: UpdateDonationBody): Promise<Donation> => {
+  try {
+    const localDonation = await sqlModels.Donation.find({ where: { id: body.id }})
+    return localDonation.destroy()
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
