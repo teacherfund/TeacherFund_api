c16b537b84a1f09d1909f548bb3b794f92859746
Joel Wasserman <joel.wasserman8@gmail.com>
2019-03-13 08:36:28 -0700

    updated routes to match FE

--- src/router.ts
+++ src/router.ts
@@ -8,11 +8,11 @@ router.get('/alive', controllers.general.alive)
 
 /* Account routes */
 // send magic link to email for login flow
-router.post('/account/reset/login', controllers.account.login)
+router.post('/account/login', controllers.account.login)
 // send magic link to user email for register flow
 router.post('/account/register', controllers.account.register)
 // receive magic link request to register or login redirect to home page auth'd
-router.post('/account/register/verify', controllers.account.verifyAuth)
+router.post('/account/verify', controllers.account.verifyAuth)
 
 /* Teacher routes */
 router.post('/donate', controllers.donation.createDonation)
